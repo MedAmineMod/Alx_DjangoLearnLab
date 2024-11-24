abd479b0e747c25f01a75a2763c26047492a6dae
MedAmineMod <modni.amine@outlook.com>
2024-11-24 20:59:20 +0100

    __str_

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,14 +5,17 @@ from django.db import models
 class Author(models.Model): 
     name  =  models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Book(models.Model): 
 
     title = models.CharField(max_length=200)
     author = models.ForeignKey(to=Author , on_delete=models.CASCADE)
 
-    # def __str__(self):
-    #     return f"Book: {self.title} by {self.author} in {self.publication_year}"  
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"  
 
 
 
