328a9a2ff8e63031a05622b31449dfcab6246b97
MedAmineMod <modni.amine@outlook.com>
2024-11-24 23:34:14 +0100

    ..

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Book , Library
+from django.views.generic import DetailView
 
 # Create your views here.
+
+
+
+def book_list(request):
+    books = Book.objects.all()  # Fetch all book instances from the database
+    context = {'books': books}  # Create a context dictionary with book list
+    return render(request, 'relationship_app/list_books.html', context)
+
+
+class LibraryDetailView(DetailView):
+   
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        """Injects additional context data specific to the book."""
+        context = super().get_context_data(**kwargs)  # Get default context data
+        library = self.get_object() # Retrieve the current book instance
+        context['library'] = library
+        return  context
